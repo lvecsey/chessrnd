e579263421d3c6ab27a799b034e236a78f7f543c
Lester Vecsey <lvecsey@gmail.com>
2017-06-06 17:50:12 -0400

    for test functions call bitmask_change through header.

--- update_game.h
+++ update_game.h
@@ -1,10 +1,14 @@
 #ifndef UPDATE_GAME_H
 #define UPDATE_GAME_H
 
+#include <stdint.h>
+
 #include "basegame.h"
 
 #include "ml.h"
 
 int update_game(basegame_t *game, struct ml *pn, char *strbuf);
+
+uuuuint64_t bitmask_change(uint64_t start, char *Strbuf);
 
 #endif
