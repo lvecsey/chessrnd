c71145937ce9732ac7db58a5f227647ab129354f
Lester Vecsey <lvecsey@gmail.com>
2017-06-07 02:23:54 -0400

    improve knight movement correctness.

--- movement_knight.c
+++ movement_knight.c
@@ -41,8 +41,10 @@ uint64_t movement_knight(uint64_t knight, uint64_t totalpop, uint64_t opp_pieces
       
       if (knight & mask) {
 
-	bitnexts[0] = setnext(bitno - 17, is_gts+0);
-
+	if (file >= 0) {
+	  bitnexts[0] = setnext(bitno - 17, is_gts+0);
+	}
+	  
 	if (file > 0) {
 	  bitnexts[1] = setnext(bitno - 15, is_gts+1);
 	}
@@ -50,18 +52,18 @@ uint64_t movement_knight(uint64_t knight, uint64_t totalpop, uint64_t opp_pieces
 	if (file > 0) {
 	  bitnexts[2] = setnext(bitno + 17, is_gts+2);
 	}
-	
-	bitnexts[3] = setnext(bitno + 15, is_gts+3);
 
+	bitnexts[3] = setnext(bitno + 15, is_gts+3);
+	  
 	if (file < 6) {
 	  bitnexts[4] = setnext(bitno - 10, is_gts+4);
 	}
 
-	if (file < 6) {
+	if (file > 1 && file < 6) {
 	  bitnexts[5] = setnext(bitno - 6, is_gts+5);
 	}
 	
-	if (file < 6) {
+	if (file > 1 && file < 6) {
 	  bitnexts[6] = setnext(bitno + 10, is_gts+6);
 	}
 
