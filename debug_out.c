b67425762f6d30ea3ad59e02dbced5a13b721af5
Lester Vecsey <lvecsey@gmail.com>
2017-06-07 17:27:06 -0400

    thorough debug_out pieceloc2.

--- debug_out.c
+++ debug_out.c
@@ -6,6 +6,10 @@
 int debug_out(basegame_t *game, struct ml *pn) {
   
   show_ml(pn);
+
+  show_pieceloc2(&game->wh2);
+  show_pieceloc2(&game->bl2);  
+  
   show_positions(game->positions);
 
   return 0;
