9558758da0ab0ac68996adc3fd5f56c44166d4e0
Lester Vecsey <lvecsey@gmail.com>
2017-06-08 15:50:10 -0400

    extended setpos gameset

--- gameset.c
+++ gameset.c
@@ -19,6 +19,8 @@ int gameset(basegame_t *game) {
 
   int retval;
 
+  long int moveno = -1;
+  
   game->wh.pawns = piecemask_wpawn;
   game->wh.rooks = piecemask_wrook;
   game->wh.bishops = piecemask_wbishop;
@@ -80,40 +82,40 @@ int gameset(basegame_t *game) {
   }
 
   for (n = 0; n < 64; n++) {
-    game->positions[n] = (pos_t) { .ptype = EPIECE, .ploc = NULL };
+    game->positions[n] = (pos_t) { .moveno_history = moveno, .ptype = EPIECE, .ploc = NULL };
   }
   
   {
     long int rank;
 
     rank = 7;
-    game->positions[rank*8+0] = setpos(BROOK, game->bl2.rooks+0);
-    game->positions[rank*8+1] = setpos(BKNIGHT, game->bl2.knights+0);
-    game->positions[rank*8+2] = setpos(BBISHOP, game->bl2.bishops+0);
-    game->positions[rank*8+3] = setpos(BQUEEN, game->bl2.queens+0);
-    game->positions[rank*8+4] = setpos(BKING, &game->bl2.king);
-    game->positions[rank*8+5] = setpos(BBISHOP, game->bl2.bishops+1);
-    game->positions[rank*8+6] = setpos(BKNIGHT, game->bl2.knights+1);
-    game->positions[rank*8+7] = setpos(BROOK, game->bl2.rooks+1);
+    game->positions[rank*8+0] = setpos(BROOK, game->bl2.rooks+0, moveno);
+    game->positions[rank*8+1] = setpos(BKNIGHT, game->bl2.knights+0, moveno);
+    game->positions[rank*8+2] = setpos(BBISHOP, game->bl2.bishops+0, moveno);
+    game->positions[rank*8+3] = setpos(BQUEEN, game->bl2.queens+0, moveno);
+    game->positions[rank*8+4] = setpos(BKING, &game->bl2.king, moveno);
+    game->positions[rank*8+5] = setpos(BBISHOP, game->bl2.bishops+1, moveno);
+    game->positions[rank*8+6] = setpos(BKNIGHT, game->bl2.knights+1, moveno);
+    game->positions[rank*8+7] = setpos(BROOK, game->bl2.rooks+1, moveno);
 
     rank = 6;
     for (file = 0; file < 8; file++) {
-      game->positions[rank*8+file] = setpos(BPAWN, game->bl2.pawns+file);
+      game->positions[rank*8+file] = setpos(BPAWN, game->bl2.pawns+file, moveno);
     }
 
     rank = 0;
-    game->positions[rank*8+0] = setpos(WROOK, game->wh2.rooks+0);
-    game->positions[rank*8+1] = setpos(WKNIGHT, game->wh2.knights+0);
-    game->positions[rank*8+2] = setpos(WBISHOP, game->wh2.bishops+0);
-    game->positions[rank*8+3] = setpos(WQUEEN, game->wh2.queens+0);
-    game->positions[rank*8+4] = setpos(WKING, &game->wh2.king);
-    game->positions[rank*8+5] = setpos(WBISHOP, game->wh2.bishops+1);
-    game->positions[rank*8+6] = setpos(WKNIGHT, game->wh2.knights+1);
-    game->positions[rank*8+7] = setpos(WROOK, game->wh2.rooks+1);
+    game->positions[rank*8+0] = setpos(WROOK, game->wh2.rooks+0, moveno);
+    game->positions[rank*8+1] = setpos(WKNIGHT, game->wh2.knights+0, moveno);
+    game->positions[rank*8+2] = setpos(WBISHOP, game->wh2.bishops+0, moveno);
+    game->positions[rank*8+3] = setpos(WQUEEN, game->wh2.queens+0, moveno);
+    game->positions[rank*8+4] = setpos(WKING, &game->wh2.king, moveno);
+    game->positions[rank*8+5] = setpos(WBISHOP, game->wh2.bishops+1, moveno);
+    game->positions[rank*8+6] = setpos(WKNIGHT, game->wh2.knights+1, moveno);
+    game->positions[rank*8+7] = setpos(WROOK, game->wh2.rooks+1, moveno);
 
     rank = 1;
     for (file = 0; file < 8; file++) {
-      game->positions[rank*8+file] = setpos(WPAWN, game->wh2.pawns+file);
+      game->positions[rank*8+file] = setpos(WPAWN, game->wh2.pawns+file, moveno);
     }    
     
   }
